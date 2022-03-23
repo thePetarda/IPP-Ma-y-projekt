adab861634dc03b55ac49574f5585b2cecb96d2a
Natalia Weronika Bednarek <01158589@pw.edu.pl>
2022-03-23 20:16:48 +0100

    load working

--- projekt.c
+++ projekt.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int load(int *n){
+int load(int** n){
     int k = 0;
     int elem = getchar();
-    if (elem == '\n') {
-        *n = malloc(sizeof(int));
-    }
-    while (elem != '\n')
+    while ((elem != '\n') && (elem != EOF))
     {
-        n[k] = elem;
+        if ((elem != ' ') && (elem != '\t') && (elem != '\v') && (elem != '\f') && (elem != '\r')) {
+            *(*n + k) = elem;
+            k ++;
+            *n = realloc(*n, (k + 1)*sizeof(int));
+        }
         elem = getchar();
-        k ++;
-        *n = realloc(n, (k + 1)*sizeof(int));
     }
     return k;
-    
 }
+
+int loadall(int** n, int** x, int** y) {
+    int a = load(n);
+    int b = load(x);
+    int c = load(y);
+    if ((a == b) && (b == c)) return a;
+    else return 0; //WYJĄTEK!!!!!!!
+}
+
 
 int main (){
     int k = 0;
-    int n[1];
-    int x[1];
-    int y[1];
+    int *n = malloc(sizeof(int));
+    int *x = malloc(sizeof(int));
+    int *y = malloc(sizeof(int));
+    k = loadall(&n, &x, &y);
+    for (int i = 0; i < k; i++){
+        putchar(n[i]);
+    }
+    // n[0] = getchar();
+    // putchar(n[0]);
     return 0;
 }
