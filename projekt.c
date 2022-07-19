7986e71e1ba73bd8367d957dde8bb568aff88deb
Natalia Weronika Bednarek <01158589@pw.edu.pl>
2022-07-19 21:10:58 +0200

    wakacje

--- projekt.c
+++ projekt.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-// check realloc in load
 // ogarnąć zmienne do użycia
+//sprawdzić czy początek i koniec to nie ściany
 
-int load(int** n, int* k){
+int load(unsigned int** n, unsigned int* k){
+    //dodane start
+    //rozkminić lepsze wczytywanie
     int elem = getchar();
-    int num = 0;
+    unsigned int num = 0;
     int space = 1;
+    int start = 1;
     while ((elem != '\n') && (elem != EOF))
     {
         if ((elem != ' ') && (elem != '\t') && (elem != '\v') && (elem != '\f') && (elem != '\r')) {
@@ -15,16 +18,17 @@ int load(int** n, int* k){
             if ((dig < 0) || (dig > 9)) {return 1;}
             num += dig;
             space = 0;
+            start = 0;
         }
         else if (space == 0){
             space = 1;
-            if (num != 0){ 
+            if (start != 1){ 
                 *(*n + (*k)) = num;
                 num = 0;
                 (*k) ++;
-                *n = realloc(*n, (*k)*sizeof(int)); //może wystarczy k
+                *n = realloc(*n, (*k)*sizeof(unsigned int)); //może wystarczy k
+                if (*n == NULL) {return 2;}
             }
-            if (*n == NULL) {return 2;}
         }
         elem = getchar();
     }
@@ -36,11 +40,11 @@ int load(int** n, int* k){
 }
 
 
-int loadhex(int** z, int k){
-    *z = realloc(*z, (k)*sizeof(int));
+int loadhex(unsigned int** z, unsigned int k){
     int elem = getchar();
-    int j = 0;//czy na końcu j == k - 1?
+    unsigned int j = 0;//czy na końcu j == k - 1?
     while ((elem != '\n') && (elem != EOF)) {
+        if (j >= k) {return 1;}
         if ((elem < 58) && (elem > 47)){
             elem -= 48;
         }
@@ -52,9 +56,7 @@ int loadhex(int** z, int k){
             elem -= 96;
             elem += 9;
         }
-        else{//ERROR 
-        }
-        printf("%d,", elem);
+        else {return 1;}
         for (int i = 0; i < 4; i++){
             *(*z + j + 3 - i) = elem % 2;
             elem /= 2;
@@ -66,74 +68,158 @@ int loadhex(int** z, int k){
 }
 
 
-int loadweird(int** z, int p, int k){
-    int *a = malloc(sizeof(int));
-    int l = load(&a); //CZY L==5? i czy m ==0?
-    int* s = malloc((a[3]) * sizeof(int));
-    int* w = malloc((a[3]) * sizeof(int));
-    s[0] = (a[0] * a[4] + a[1]) % a[2] + 1; //czy +1?
-    for (int i = 1; i < a[3]; i++){
-        s[i] = (a[0] * s[i - 1] + a[1]) % a[2] + 1; //czy + 1?
+int loadweird(unsigned int** z, unsigned int p, unsigned int k){
+    unsigned int *a = malloc(sizeof(unsigned int));
+    int l = 0;
+    int t = load(&a, &l);
+    if ((l != 5) || (t != 0)) {return 1;}
+    if (a[2] == 0) {return 1;} // m != 0
+    unsigned int* s = malloc((a[3]) * sizeof(unsigned int));
+    unsigned int* w = malloc((a[3]) * sizeof(unsigned int));
+    s[0] = (a[0] * a[4] + a[1]) % a[2] + 1;
+    for (unsigned int i = 1; i <= a[3]; i++){
+        // s[i] = (a[0] * s[i - 1] + a[1]) % a[2] + 1;
+        s[i] = (a[0] * s[i - 1] + a[1]) % a[2];
     }
-    for (int i = 0; i <a[3]; i++){
+    for (unsigned int i = 0; i < a[3]; i++){
         w[i] = s[i] % p;
-        int elem = w[i];
-        while (elem < k) {
-            *z[elem - 1] = 1;
-            elem += 4294967296;
-        }
+        unsigned int elem = w[i];
+        // while (elem < k) {
+        //     *(*z + (elem - 1)) = 1;
+        //     elem += 4294967296;
+        // }
+        *(*z + (elem - 1)) = 1;
     }
+    free(a);
+    free(s);
+    free(w);
     return 0;
 }
 
 
-int loadz(int** z, int p, int k){
+int loadz(unsigned int** z, unsigned int p, unsigned int k){
+    int t = 0;
+    //jaki ma być rozmiar z?
+    *z = realloc(*z, (k)*sizeof(unsigned int));
+    if (*z == NULL) {return 2;}
+    for (unsigned int i = 0; i < k; i++) {*(*z + i) = 0;}
     int elem1 = getchar();
-    *z = realloc(*z, (k)*sizeof(int));
-    for (int i = 0; i < k; i++) {*(*z + i) = 0;}
+    // putchar(elem1);
     if (elem1 == 'R'){
-        loadweird(z, p, k);
+        // printf("LOL");
+        t = loadweird(z, p, k);
     }
     else {
         int elem2 = getchar();
         if ((elem1 == '0') && (elem2 == 'x')){
-            loadhex(z, k);
+            t = loadhex(z, k);
         }
-        else {}
+        else {return 1;}
     }
+    return t;
+}
+
+int checkempty(unsigned int** z, unsigned int** n, unsigned int** q, unsigned int k){
+    int a = 0;
+    int p = 1;
+    for (int i = 0; i < k; i++){
+        int el = *(*q + i);
+        if (el > *(*n + i)){return 2;}
+        a += (el - 1)*p;
+        p *= *(*n + i);
+    }
+    // check rozmiar a
+    return *(*z + a);
 }
 
 
-int loadall(int** n, int** x, int** y, int** z, int* k) {
+int loadall(unsigned int** n, unsigned int** x, unsigned int** y, unsigned int** z, unsigned int* k) {
     int t;
     t = load(n, k);
-    if (t != 0) {return 1;}
-    int a = *k;
+    if (t == 2) {return 6;}
+    if ((t != 0) || ((*k) == 0)) {return 1;}
+    unsigned int a = *k;
     *k = 0;
     t = load(x, k);
+    if (t == 2) {return 6;}
     if ((a != *k) || (t != 0)) {return 2;}
     *k = 0;
     t = load(y, k);
+    if (t == 2) {return 6;}
     if ((a != *k) || (t != 0)) {return 3;}
-    // int p = 1;
-    // for (int i = 0; i < a; i++){p *= *n[i];}
-    // loadz(z, p, *k);
+    unsigned int m = 0;
+    unsigned int p = 1;
+    for (unsigned int i = 0; i < (*k); i++){
+        unsigned int x = (*(*n + i)) - 1;
+        p *= (*(*n + i));
+        for (unsigned int j = 0; j < i; j++) {
+            x *= (*(*n + j));
+        }
+        m += x;
+    }
+    if ((m % 4) != 0) {m += 4 - (m % 4);}
+    t = loadz(z, p, *k); //wcześniej *k -> m
+    if (t == 2) {return 6;}
+    if (t != 0) {return 4;}
+    if (getchar() != EOF) {return 5;}
     return 0;
+}
+
+
+void end(unsigned int** n, unsigned int** x, unsigned int** y, unsigned int** z){
+    free(*n);
+    free(*x);
+    free(*y);
+    free(*z);
 }
 
 
 int main (){
-    int k = 0;
-    int *n = malloc(sizeof(int));
-    int *x = malloc(sizeof(int));
-    int *y = malloc(sizeof(int));
-    int *z = malloc(sizeof(int));
-    loadall(&n, &x, &y, &z, &k);
-    for (int i = 0; i < k; i++){
-        // putchar(n[i]);
-        printf("%d,", n[i]);
+    unsigned int k = 0;
+    unsigned int *n = malloc(sizeof(unsigned int));
+    unsigned int *x = malloc(sizeof(unsigned int));
+    unsigned int *y = malloc(sizeof(unsigned int));
+    unsigned int *z = malloc(sizeof(unsigned int));
+    int t = loadall(&n, &x, &y, &z, &k);
+    if (t != 0) {
+        printf("ERROR %d\n", (t % 6));
+        end(&n, &x, &y, &z);
+        return 1;
     }
-    // n[0] = getchar();
-    // putchar(n[0]);
+    end(&n, &x, &y, &z);
+    printf("NICE");
     return 0;
 }
+
+
+// printf("%d,\n", k);
+    // for (unsigned int i = 0; i < k; i++){
+    //     // putchar(n[i]);
+    //     printf("%d,", n[i]);
+    // }
+    // printf("\n");
+    // for (unsigned int i = 0; i < k; i++){
+    //     // putchar(n[i]);
+    //     printf("%d,", x[i]);
+    // }
+    // printf("\n");
+    // for (unsigned int i = 0; i < k; i++){
+    //     // putchar(n[i]);
+    //     printf("%d,", y[i]);
+    // }
+    // printf("\n");
+    // unsigned int m = 0;
+    // for (unsigned int i = 0; i < k; i++){
+    //     unsigned int x = n[i] - 1;
+    //     for (int j = 0; j < i; j++) {
+    //         x *= n[j];
+    //     }
+    //     m += x;
+    // }
+    
+    // if ((m % 4) != 0) {m += 4 - (m % 4);}
+    // printf("%d,\n", m);
+    // for (unsigned int i = 0; i < m; i++){
+    //     printf("%d,", z[i]);
+    // }
+    // printf("\n");
--- try.c
+++ try.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+// ogarnąć zmienne do użycia
 
 int load(int** n, int* k){
     int elem = getchar();
@@ -21,8 +22,8 @@ int load(int** n, int* k){
                 num = 0;
                 (*k) ++;
                 *n = realloc(*n, (*k)*sizeof(int)); //może wystarczy k
+                if (*n == NULL) {return 2;}
             }
-            if (*n == NULL) {return 2;}
         }
         elem = getchar();
     }
@@ -33,36 +34,71 @@ int load(int** n, int* k){
     return 0;
 }
 
-int loadall(int** n, int** x, int** y, int** z, int* k) {
-    int t;
-    t = load(n, k);
-    if (t != 0) {return 1;}
-    int a = *k;
-    *k = 0;
-    t = load(x, k);
-    if ((a != *k) || (t != 0)) {return 2;}
-    *k = 0;
-    t = load(y, k);
-    if ((a != *k) || (t != 0)) {return 3;}
-    // int p = 1;
-    // for (int i = 0; i < a; i++){p *= *n[i];}
-    // loadz(z, p, *k);
+
+int loadhex(int** z, int k){
+    int elem = getchar();
+    int j = 0;//czy na końcu j == k - 1?
+    while ((elem != '\n') && (elem != EOF)) {
+        if ((elem < 58) && (elem > 47)){
+            elem -= 48;
+        }
+        else if ((elem < 71) && (elem > 64)){
+            elem -= 64;
+            elem += 9;
+        }
+        else if ((elem < 103) && (elem > 96)){
+            elem -= 96;
+            elem += 9;
+        }
+        else {return 1;}
+        for (int i = 0; i < 4; i++){
+            *(*z + j + 3 - i) = elem % 2;
+            elem /= 2;
+        }
+        j += 4;
+        elem = getchar();
+    }
+    if (j != (k - 1)) {return 1;}
     return 0;
 }
 
-int main () {
+
+int loadweird(int** z, int p, unsigned int k){
+    int *a = malloc(sizeof(int));
+    int l = 0;
+    int t = load(&a, &l);
+    if ((l != 5) || (t != 0)) {return 1;}
+    int* s = malloc((a[3]) * sizeof(int));
+    int* w = malloc((a[3]) * sizeof(int));
+    s[0] = (a[0] * a[4] + a[1]) % a[2] + 1;
+    for (int i = 1; i <= a[3]; i++){
+        s[i] = (a[0] * s[i - 1] + a[1]) % a[2] + 1;
+    }
+    for (int i = 0; i <a[3]; i++){
+        w[i] = s[i] % p;
+        unsigned int elem = w[i];
+        // while (elem < k) {
+        //     *(*z + (elem - 1)) = 1;
+        //     elem += 4294967296;
+        // }
+        *(*z + (elem - 1)) = 1;
+    }
+    return 0;
+}
+
+int main (){
     int k = 0;
     int *n = malloc(sizeof(int));
     int *x = malloc(sizeof(int));
     int *y = malloc(sizeof(int));
-    int *z = malloc(sizeof(int));
-    int a = loadall(&n, &x, &y, &z, &k);
-    printf("%d,", a);
-    printf("%d,\n", k);
-    for (int i = 0; i < k; i++){
+    int *z = malloc(124 * sizeof(int));
+    loadweird(&z, 125, 124);
+    for (int i = 0; i < 124; i++){
         // putchar(n[i]);
-        printf("%d,", n[i]);
+        printf("%d,", z[i]);
     }
-
+    printf("\n");
+    // n[0] = getchar();
+    // putchar(n[0]);
     return 0;
 }
